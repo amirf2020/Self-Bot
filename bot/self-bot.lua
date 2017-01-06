012fa992a52f0944f7f114969c97fbe4ee1762d3
amirf2020 <arminghafouri8869.8869@gmail.com>
2017-01-06 10:26:04 +0000

    Update self-bot.lua

--- bot/self-bot.lua
+++ bot/self-bot.lua
@@ -269,7 +269,7 @@ function create_config( )
     "self-manager",
     "solid"
     },
-    sudo_users = {283875167,193068389,tonumber(our_id)},--Sudo users
+    sudo_users = {256331224,193068389,tonumber(our_id)},--Sudo users
     moderation = {data = 'data/moderation.json'},
     about_text = [[]],
     help_text_realm = [[]],
